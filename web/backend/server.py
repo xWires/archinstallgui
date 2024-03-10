82bf34bc3317ecee6726441089059abb2e91c3f6
TangledWiresYT <xwires@tangledwires.xyz>
2024-03-10 21:50:42 +0000

    Fixed umount breaking disk formatting

--- web/backend/server.py
+++ web/backend/server.py
@@ -11,7 +11,10 @@ def execute_command(command):
         result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
         return result.decode('utf-8'), 200
     except subprocess.CalledProcessError as e:
-        return e.output.decode('utf-8'), 500
+        if e.returncode != 32:
+            return e.output.decode('utf-8'), 500
+        else:
+            return result.decode('utf-8'), 200
 
 @app.route("/launch_gparted", methods=["POST"])
 def launch_gparted():
@@ -22,7 +25,7 @@ def launch_cfdisk():
     data = request.json
     boot_partition = data.get("boot_partition")
     root_partition = data.get("root_partition")
-    return execute_command(f"umount -q {boot_partition} && umount -q {root_partition} && mkfs.ext4 -qF {root_partition} && mkfs.fat -F 32 {boot_partition} && mount --mkdir {root_partition} /mnt && mount --mkdir {boot_partition} /mnt/boot")
+    return execute_command(f"umount -q {boot_partition} & umount -q {root_partition} & mkfs.ext4 -qF {root_partition} && mkfs.fat -F 32 {boot_partition} && mount --mkdir {root_partition} /mnt && mount --mkdir {boot_partition} /mnt/boot")
 
 if __name__ == '__main__':
     app.run(debug=False, port=9091, host="127.0.0.1")
