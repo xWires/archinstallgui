a1ccfb5d8c6c677462f515fa250a62837639dbaa
TangledWiresYT <xwires@tangledwires.xyz>
2024-03-10 15:03:16 +0000

    Added gparted button

--- web/backend/server.py
+++ web/backend/server.py
@@ -5,15 +5,17 @@ import subprocess
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/execute_command', methods=['POST'])
-def execute_command():
+@app.route("/execute_command", methods=["POST"])
+def execute_command(command):
     try:
-        data = request.json
-        command = data.get('command')
         result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
         return result.decode('utf-8'), 200
     except subprocess.CalledProcessError as e:
         return e.output.decode('utf-8'), 500
+
+@app.route("/launch_gparted", methods=["POST"])
+def launch_gparted():
+    return execute_command("gparted")
 
 if __name__ == '__main__':
     app.run(debug=False, port=9091, host="127.0.0.1")
